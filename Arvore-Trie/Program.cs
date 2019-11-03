294f1e316cc9a6fd820e51524e3a6e6ec7ebe505
Gabriel Alejandro L. M <35781604+AlfafaWolf@users.noreply.github.com>
2019-11-03 13:06:17 -0300

    Adicionar arquivos de projeto.

---
+++ Arvore-Trie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arvore_Trie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
